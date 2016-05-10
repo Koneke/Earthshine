f7f2db89cf8c0ef6ad4f66b05718ede9c9e33f03
Lukas Hagman <Lukas.Hagman@Multisoft.se>
2016-05-10 15:50:49 +0200

    cleaning, moving things about

---
+++ Earthshine/Chromosome.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Earthshine
+{
+	public class Chromosome
+	{
+		public Stat Stat;
+		public float Value;
+
+		public Chromosome(Stat stat, float value)
+		{
+			this.Stat = stat;
+			this.Value = value;
+		}
+
+		public static Chromosome Generate(Stat? specificStat = null)
+		{
+			var stat = specificStat ?? Stats.GetRandomStat();
+			return new Chromosome(stat, Program.Random.Next(10, 20));
+		}
+
+		public override string ToString()
+		{
+			var statName = Enum.GetName(typeof(Stat), this.Stat);
+			statName = statName.Substring(0, 2);
+			return $"{statName}: {this.Value}";
+		}
+	}
+}
---
+++ Earthshine/DNA.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Earthshine
+{
+	public class DNA
+	{
+		public static Comparer<Chromosome> Comparer = Comparer<Chromosome>.Create((a, b) => a.Value.CompareTo(b.Value));
+
+		public Dictionary<Stat, List<Chromosome>> Chromosomes;
+
+		public int ChromosomeCount =>
+			this.Chromosomes
+				.Keys
+				.Select(k => this.Chromosomes[k].Count)
+				.Sum();
+
+		public DNA()
+		{
+			this.Chromosomes = new Dictionary<Stat, List<Chromosome>>();
+
+			foreach (var stat in Stats.AllStats)
+			{
+				this.Chromosomes.Add(stat, new List<Chromosome>());
+			}
+		}
+
+		public static DNA GenerateRandom(int length)
+		{
+			var result = new DNA();
+
+			if (length < Stats.AllStats.Count)
+			{
+				throw new Exception("Are you trying to create a cripple??");
+			}
+
+			// guarantee atleast one chromosome per stat.
+			foreach (var stat in Stats.AllStats)
+			{
+				result.AddChromosome(Chromosome.Generate(stat));
+			}
+
+			// rest we just fill up at random.
+			while (result.ChromosomeCount < length)
+			{
+				result.AddChromosome(Chromosome.Generate());
+			}
+
+			// (this is where we'd tweak things later to make it a bit more
+			// even and nice).
+
+			return result;
+		}
+
+		public static DNA Join(DNA first, DNA second)
+		{
+			if (first.ChromosomeCount != second.ChromosomeCount)
+			{
+				throw new Exception("Are you spawning a mutant? What the fuck?");
+			}
+
+			var expectedLength = first.ChromosomeCount;
+			var result = new DNA();
+
+			var firstBucket = new Dictionary<Stat, List<Chromosome>>();
+			var secondBucket = new Dictionary<Stat, List<Chromosome>>();
+			var excessBucket = new List<Chromosome>();
+
+			// create our buckets
+			foreach (var stat in Stats.AllStats)
+			{
+				firstBucket.Add(stat, new List<Chromosome>());
+				secondBucket.Add(stat, new List<Chromosome>());
+
+				first.Chromosomes[stat].ForEach(c => firstBucket[stat].Add(c));
+				second.Chromosomes[stat].ForEach(c => secondBucket[stat].Add(c));
+
+				List<Chromosome> largerPool, smallerPool;
+
+				// identify smallest/largest pool.
+				if (firstBucket[stat].Count > secondBucket[stat].Count)
+				{
+					largerPool = firstBucket[stat];
+					smallerPool = secondBucket[stat];
+				}
+				else if (firstBucket[stat].Count < secondBucket[stat].Count)
+				{
+					smallerPool = firstBucket[stat];
+					largerPool = secondBucket[stat];
+				}
+				else
+				{
+					largerPool = Program.Random.NextDouble() > 0.5
+						? firstBucket[stat]
+						: secondBucket[stat];
+					smallerPool = (largerPool == firstBucket[stat])
+						? secondBucket[stat]
+						: firstBucket[stat];
+				}
+
+				// eliminate excess so we can pair evenly.
+				// at the moment we just remove the lowest value chrom.
+				largerPool.Sort(Comparer);
+				smallerPool.Sort(Comparer);
+
+				while (largerPool.Count > smallerPool.Count)
+				{
+					excessBucket.Add(largerPool[0]);
+					largerPool.RemoveAt(0);
+				}
+			}
+
+			// join the two, now even strands, at random.
+			foreach (var stat in Stats.AllStats)
+			{
+				for (var i = 0; i < firstBucket[stat].Count; i++)
+				{
+					var chrom = Program.Random.NextDouble() > 0.5
+						? firstBucket[stat][i]
+						: secondBucket[stat][i];
+					result.Chromosomes[stat].Add(chrom);
+				}
+			}
+
+			// fill up to expected length at random from the excess bucket.
+			while (result.ChromosomeCount < expectedLength)
+			{
+				var chrom = excessBucket[Program.Random.Next(excessBucket.Count)];
+				result.Chromosomes[chrom.Stat].Add(chrom);
+			}
+
+			return result;
+		}
+
+		public void AddChromosome(Chromosome chromosome)
+		{
+			this.Chromosomes[chromosome.Stat].Add(chromosome);
+		}
+
+		public override string ToString()
+		{
+			var stats = new Dictionary<Stat, List<string>>();
+			var joined = new List<string>();
+
+			foreach (var stat in Stats.AllStats)
+			{
+				stats.Add(stat, new List<string>());
+
+				var chromosomes = new List<Chromosome>(this.Chromosomes[stat]);
+				chromosomes.Sort(Comparer);
+
+				foreach (var chromosome in chromosomes)
+				{
+					stats[stat].Add(chromosome.ToString());
+				}
+
+				joined.Add(string.Join(" ", stats[stat]));
+			}
+
+			return string.Join("      ", joined);
+		}
+	}
+}
